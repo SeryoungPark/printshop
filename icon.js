0fde1828bf8f227c6961397e49843192dfea0a12
SeryoungPark <76822758+SeryoungPark@users.noreply.github.com>
2022-07-31 00:35:35 +0900

    Delete icon.js

--- icon.js
+++
@@ -1,7 +0,0 @@
-var arr = ["result1.html","result2.html","result3.html","result4.html"
-];
-
-function randSite(){
-        location.href = "" + arr[Math.floor (Math.random()*arr.length)];
-}
-// JavaScript Document
